c5af2cf0b65b00253122c99c1e7c6970cea74d19
devilsown1410 <rahulsingh231841@gmail.com>
2025-05-26 11:31:55 +0530

    Add user deletion functionality and authorization check in admin panel

--- Admin/app.js
+++ Admin/app.js
@@ -1,10 +1,17 @@
 const User = JSON.parse(localStorage.getItem("User"));
 const userList = document.getElementById("userTable");
 const home = document.getElementById("home");
+const currentUser = JSON.parse(sessionStorage.getItem("curr_user"))?.isAdmin;
 home.addEventListener("click", function () {
   window.location.href = "../index.html";
 });
 function renderUsers() {
+  console.log(currentUser);
+  if (currentUser !== true) {
+    alert("You are not authorized to view this page.");
+    sessionStorage.removeItem("curr_user");
+    window.location.href = "../Login/login.html";
+  }
   if( User.length === 0) {
     userList.innerHTML = "<tr><td colspan='7'>No users found</td></tr>";
     return;
@@ -21,6 +28,7 @@ function renderUsers() {
     const tableData5 = document.createElement("td");
     const tableData6 = document.createElement("td");
     const tableData7 = document.createElement("td");
+    const tableData8 = document.createElement("td");
     tableData1.innerText = element.username;
     tableData3.innerText = element.email;
     tableData2.innerText = element.password;
@@ -28,6 +36,17 @@ function renderUsers() {
     tableData5.innerText = element.gender;
     tableData6.innerText = element.phone;
     tableData7.innerText = element.hobbies;
+    tableData8.innerHTML = `
+      <button class="btn btn-danger" id="delete">❌</button>`;
+    tableData8.addEventListener("click", function (e) {
+      const userIndex = User.findIndex((user) => user.username === element.username);
+      if (userIndex !== -1) {
+        User.splice(userIndex, 1);
+        localStorage.setItem("User", JSON.stringify(User));
+        alert("User deleted successfully");
+        window.location.reload();
+      }
+    });
     tableRow.appendChild(tableData1);
     tableRow.appendChild(tableData2);
     tableRow.appendChild(tableData3);
@@ -35,6 +54,7 @@ function renderUsers() {
     tableRow.appendChild(tableData5);
     tableRow.appendChild(tableData6);
     tableRow.appendChild(tableData7);
+    tableRow.appendChild(tableData8);
     userList.appendChild(tableRow);
   });
 }
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const curr_user = document.querySelector("#curr_user");
 curr_user.textContent = JSON.parse(
   sessionStorage.getItem("curr_user")
 )?.username;
+
 if (!curr_user.textContent) {
   alert("Please login first");
   window.location.href = "/Login/login.html";
